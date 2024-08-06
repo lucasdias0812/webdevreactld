73484b62da91b76748b5504a7a8b42fb603efe6c
lucasdias0812 <99883219+lucasdias0812@users.noreply.github.com>
2024-08-06 20:49:20 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Titulo from './components/Titulo'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <Titulo/>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 10000000000)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Titulo.jsx
@@ -0,0 +1,7 @@
+function Titulo(){
+    return(
+        <h1> Hello World </h1>
+    )
+}
+
+export default Titulo
