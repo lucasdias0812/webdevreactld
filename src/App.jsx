1af55fca863d61b54e030715ca75a452a49246a0
lucasdias0812 <99883219+lucasdias0812@users.noreply.github.com>
2024-08-13 21:25:36 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Titulo from './components/Titulo'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import InformacoesPessoais from './components/InformacoesPessoais'
+import Contato from './components/Contato'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <Titulo/>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 10000000000)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Header/>
+    <div className='mainInfo'>
+    <InformacoesPessoais/>
+    <Contato/>
+    </div>
+    <Footer/>
     </>
   )
 }
